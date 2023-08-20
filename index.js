9288c6f96779056663180b40325a795237198dca
salma <salmasbaiti1@gmail.com>
2023-08-21 00:42:38 +0100

    test1 & 2

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ app.get("/", function (req, res) {
 });
 
 
+// date to UNIX
+app.get("/api/:date?", function (req, res) {
+  let date =  new Date(req.params.date );
+  
+  if(!req.params.date){
+    res.json({unix : new Date()})
+  }else{
+    let unix =  date.getTime()
+    let utc = date.getDate()
+    res.json({unix , utc});
+  }
+  
+});
+
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
@@ -27,6 +42,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
